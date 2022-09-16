3829f9f5adee946a813c9e73edf48bde67025c8f
Fab-c <71762204+sidrhat@users.noreply.github.com>
2022-09-16 04:05:52 -0700

    Add files via upload
    As here you can see i have not used some images here but we can also use them in our project

---
+++ send.php
@@ -0,0 +1,7 @@
+<?php
+$name = $_POST["name"];
+echo" <h1 color='red' align='center'>You are registered for our Newsteller</h1>
+<h1 color='blue' align='center'>$name</h1>
+<h1 color='yellow' align='center'>Thanks for registering with us </h1> ";
+?>
+
